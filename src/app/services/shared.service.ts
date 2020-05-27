09922eeb8f644abc201690e65a9b39cf33867443
vatsal199 <vatsal17137@gmail.com>
2020-05-27 23:51:01 +0530

    mail added

--- src/app/services/shared.service.ts
+++ src/app/services/shared.service.ts
@@ -5,7 +5,8 @@ import { Injectable } from '@angular/core';
 })
 export class SharedService {
 
-  private domainName:string = 'http://127.0.0.1:8090/';
+  //private domainName:string = 'http://127.0.0.1:8090/';
+  private domainName:string = 'http://172.17.0.2:8090/';
 
   constructor() { }
 
